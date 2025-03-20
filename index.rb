29b03bdc46a17c1a485c2db2d007ab475974fd46
Ramatou81 <karimouseyniramatou97@gmail.com>
2025-03-20 10:54:22 +0100

    Add files via upload

---
+++ index.rb
@@ -0,0 +1,25 @@
+# Définition des valeurs de couleur
+COLOR_CODES = {
+  "black" => 0,
+  "brown" => 1,
+  "red" => 2,
+  "orange" => 3,
+  "yellow" => 4,
+  "green" => 5,
+  "blue" => 6
+}
+
+def resistor_value(colors)
+  digits = colors.map { |color| COLOR_CODES[color] }.compact
+  value = digits.first(2).join
+  value.empty? ? "Invalid input" : value
+end
+
+puts "Enter resistor colors separated by space:"
+input_colors = gets.chomp.split
+puts "Resistor value: #{resistor_value(input_colors)}"
+
+
+
+
+
